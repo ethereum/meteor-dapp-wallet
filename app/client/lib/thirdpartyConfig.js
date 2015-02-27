ad194ddba10faa2b052af814456030d64bfa8d75
Fabian Vogelsteller <fabian@frozeman.de>
2015-02-27 14:28:15 +0100

    add new ethereuem.js version

--- app/client/lib/thirdpartyConfig.js
+++ app/client/lib/thirdpartyConfig.js
@@ -1,9 +1,3 @@
 
-// get the web3 object
-if(typeof web3 === 'undefined') {
-    web3  = require('web3');
-
-}
-
 // set providor
 web3.setProvider(new web3.providers.HttpSyncProvider("http://localhost:8545")); //8545 8080 10.10.42.116
