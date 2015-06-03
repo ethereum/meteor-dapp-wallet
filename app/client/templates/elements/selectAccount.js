7c91b2d1a4ff1d25134e2aaee26e93a76ab046a2
Fabian Vogelsteller <fabian@frozeman.de>
2015-06-03 18:42:17 +0200

    created select account component

---
+++ app/client/templates/elements/selectAccount.js
@@ -0,0 +1,49 @@
+/**
+Template Controllers
+
+@module Templates
+*/
+
+/**
+The select account template
+
+@class [template] elements_selectAccount
+@constructor
+*/
+
+Template['elements_selectAccount'].onCreated(function(){
+    if(this.data && this.data.accounts) {
+        TemplateVar.set('selectedAccount', this.data.accounts.fetch()[0].address);
+    }
+});
+
+
+Template['elements_selectAccount'].helpers({
+    /**
+    Use this method, to overwrite the global accounts helper.
+
+    @method (accounts)
+    */
+    'accounts': function(){
+        return this.accounts;
+    },
+    /**
+    Return the selected address
+
+    @method (selectedAccount)
+    */
+    'selectedAccount': function(){
+        return TemplateVar.get('selectedAccount');
+    },
+});
+
+Template['elements_selectAccount'].events({
+    /**
+    Set the selected address.
+    
+    @event change select[name="select-accounts"]
+    */
+    'change select[name="select-accounts"]': function(e){
+        TemplateVar.set('selectedAccount', e.currentTarget.value);
+    }
+});
