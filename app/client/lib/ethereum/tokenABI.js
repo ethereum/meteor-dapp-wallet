43a5115b8e04de429a851ac552f4e57085d26d08
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2015-10-06 18:37:39 -0300

    added tokenABI.js

---
+++ app/client/lib/ethereum/tokenABI.js
@@ -0,0 +1,4 @@
+
+
+// Token Interface
+tokenABI = [{constant:false,inputs:[{name:'receiver',type:'address'},{name:'amount',type:'uint256'}],name:'sendCoin',outputs:[{name:'sufficient',type:'bool'}],type:'function'},{constant:true,inputs:[{name:'',type:'address'}],name:'coinBalanceOf',outputs:[{name:'',type:'uint256'}],type:'function'},{inputs:[{name:'supply',type:'uint256'}],type:'constructor'},{anonymous:false,inputs:[{indexed:false,name:'sender',type:'address'},{indexed:false,name:'receiver',type:'address'},{indexed:false,name:'amount',type:'uint256'}],name:'CoinTransfer',type:'event'}];
