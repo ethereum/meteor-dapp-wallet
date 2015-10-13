1ef7f01f2173b486543dd03b90b2f5b37b608dfe
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2015-10-13 18:32:22 -0300

    added modal

---
+++ app/client/templates/views/modals/addToken.js
@@ -0,0 +1,18 @@
+/**
+The template to display qrCode.
+
+@class [template] views_account
+@constructor
+*/
+
+
+Template['views_modals_addToken'].events({
+    /**
+    Submit form
+
+    @event submit form
+    */
+    // '.decimal': change(e) {
+        
+    // }
+});
