46a670c4d1f5971fd68a05696ec2ea26a846084d
Fabian Vogelsteller <fabian@frozeman.de>
2016-03-14 14:59:58 +0100

    use proper way to initialize web3.js

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -1,3 +1,6 @@
 // set providor
-if(typeof web3 === 'undefined')
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")); //8545 8080 10.10.42.116
+if(typeof web3 !== 'undefined')
+  web3 = new Web3(web3.currentProvider);
+else
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  
