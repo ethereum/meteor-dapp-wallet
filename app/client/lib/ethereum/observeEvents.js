f5e04fe037785c43013345cc6a2a931495aa8a72
Alex Van de Sande <alex.vandesande@ethdev.com>
2016-05-09 17:50:51 -0300

    Improvements on event logging for very active contracts (#165)
    * reduced the amount of database reads from the observeEvents function
    
    * Simplified even observe
    
    * removed extraneous log
    
    * removed extra timer
    
    * added an extra write, as suggested by Bas

--- app/client/lib/ethereum/observeEvents.js
+++ app/client/lib/ethereum/observeEvents.js
@@ -1,3 +1,5 @@
+// meant to speed up and make less requests
+var customContractsCache = {};
 /**
 Observe events
 
@@ -12,15 +14,31 @@ observeEvents = function(){
     */
     collectionObservers[collectionObservers.length] = Events.find({}).observe({
         /**
-        This will observe the transactions creation and create watchers for outgoing transactions, to see when they are mined.
+        This will observe when events are added and link it to the custom contract.
 
         @method added
         */
         added: function(newDocument) {
+            
+            // This creates a temporary cache for the contracts, to reduce the amount of db read and writes            
+            var customContract = customContractsCache[newDocument.address.toLowerCase()];
+
+            if (typeof customContract == 'undefined') {
+                var customContract = CustomContracts.findOne({address: newDocument.address.toLowerCase()});
+                customContractsCache[newDocument.address.toLowerCase()] = customContract; 
+            }
+
             // add to accounts
-            CustomContracts.update({address: newDocument.address.toLowerCase()}, {$addToSet: {
-                contractEvents: newDocument._id
-            }});
+            if (customContract.contractEvents.indexOf(newDocument._id)<0) {
+                // Only if the event isn't there
+                CustomContracts.update({address: newDocument.address.toLowerCase()}, {$addToSet: {
+                    contractEvents: newDocument._id
+                }});  
+    
+                customContract.contractEvents.push(newDocument._id);    
+                customContractsCache[newDocument.address.toLowerCase()] = customContract; 
+            }
+
         },
         /**
         Remove events confirmations from the accounts
