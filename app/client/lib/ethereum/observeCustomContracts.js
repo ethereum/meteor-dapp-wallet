5764e436b0dae15b34a9e545e6b5cce930a1c7e3
Alex Van de Sande <alex.vandesande@ethdev.com>
2016-06-07 11:58:01 -0300

    Update Master Branch (#215)
    * Added Korean translation and changes to french
    
    * Fixes issue that it would not copy to clipboard on the main net
    
    * Removed events subscription for performance reasons
    
    * Fixed header jump on transparent bar (Mist Browser only)
    
    * Updates Dapp styles to fix a minor icon clipping issue
    
    * Fixes incorrect total token balance (#206 and https://github.com/ethereum/mist/issues/608 )
    
    * Adds build instructions
    
    * Make the wallet span the whole width of the app (#212)
    
    * When sending from multisig, makes sure you own the "from" account (#210). This makes sure not only that you have that account, but it picks the highest balance you have

--- app/client/lib/ethereum/observeCustomContracts.js
+++ app/client/lib/ethereum/observeCustomContracts.js
@@ -10,13 +10,12 @@ var addLogWatching = function(newDocument){
     // console.log('EVENT LOG:  Checking Custom Contract Events for '+ newDocument.address +' (_id: '+ newDocument._id + ') from block # '+ blockToCheckBack);
 
     // delete the last logs until block -500
-    _.each(Events.find({_id: {$in: newDocument.contractEvents || []}, blockNumber: {$exists: true, $gt: blockToCheckBack}}).fetch(), function(log){
-        if(log)
-            Events.remove({_id: log._id});
-    });
+    // _.each(Events.find({_id: {$in: newDocument.contractEvents || []}, blockNumber: {$exists: true, $gt: blockToCheckBack}}).fetch(), function(log){
+    //     if(log)
+    //         Events.remove({_id: log._id});
+    // });
 
-    var filter = contractInstance.allEvents({fromBlock: blockToCheckBack, toBlock: 'latest'});
-    // events.push(filter);
+    // var filter = contractInstance.allEvents({fromBlock: blockToCheckBack, toBlock: 'latest'});
     
     // get past logs, to set the new blockNumber
     var currentBlock = EthBlocks.latest.number;
@@ -29,31 +28,31 @@ var addLogWatching = function(newDocument){
         }
     });
 
-    filter.watch(function(error, log){
-        if(!error) {
-            var id = Helpers.makeId('log', web3.sha3(log.logIndex + 'x' + log.transactionHash + 'x' + log.blockHash));
+    // filter.watch(function(error, log){
+    //     if(!error) {
+    //         var id = Helpers.makeId('log', web3.sha3(log.logIndex + 'x' + log.transactionHash + 'x' + log.blockHash));
 
-            if(log.removed) {
-                Events.remove(id);
-            } else {
-                web3.eth.getBlock(log.blockHash, function(err, block){
-                    if(!err) {
+    //         if(log.removed) {
+    //             Events.remove(id);
+    //         } else {
+    //             web3.eth.getBlock(log.blockHash, function(err, block){
+    //                 if(!err) {
 
-                        _.each(log.args, function(value, key){
-                            // if bignumber
-                            if((_.isObject(value) || value instanceof BigNumber) && value.toFormat) {
-                                value = value.toString(10);
-                                log.args[key] = value;
-                            }
-                        });
+    //                     _.each(log.args, function(value, key){
+    //                         // if bignumber
+    //                         if((_.isObject(value) || value instanceof BigNumber) && value.toFormat) {
+    //                             value = value.toString(10);
+    //                             log.args[key] = value;
+    //                         }
+    //                     });
 
-                        log.timestamp = block.timestamp;
-                        Events.upsert(id, log);
-                    }
-                });
-            }
-        }
-    });
+    //                     log.timestamp = block.timestamp;
+    //                     Events.upsert(id, log);
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
 };
 
 /**
@@ -83,7 +82,7 @@ observeCustomContracts = function(){
                         disabled: false
                     }});  
                     // check for logs
-                    addLogWatching(newDocument);                      
+                    // addLogWatching(newDocument);                      
                     
                 } else if (!e) {
                     // if there's no code, check the contract has a balance
@@ -93,7 +92,7 @@ observeCustomContracts = function(){
                                 disabled: false
                             }});
                             // check for logs
-                            addLogWatching(newDocument);                        
+                            // addLogWatching(newDocument);                        
                         } else if (!e) {
                             CustomContracts.update(newDocument._id, {$set: {
                                 disabled: true
