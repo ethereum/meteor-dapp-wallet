339ac7fdeb5f70956c6fb25d2176998101dc74b4
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-08-08 17:59:05 -0300

    reverse sort

--- app/client/lib/helpers/templateHelpers.js
+++ app/client/lib/helpers/templateHelpers.js
@@ -113,11 +113,11 @@ Returns a list of accounts and wallets sorted by balance
 @method (latestBlock)
 **/
 Template.registerHelper('selectAccounts', function(hideWallets){
-    var accounts = EthAccounts.find({balance:{$ne:"0"}}, {sort: {balance: -1}}).fetch();
+    var accounts = EthAccounts.find({balance:{$ne:"0"}}, {sort: {balance: 1}}).fetch();
     
     if(hideWallets !== true)
         accounts = _.union(Wallets.find({owners: {$in: _.pluck(accounts, 'address')}, address: {$exists: true}}, {sort: {name: 1}}).fetch(), accounts);
-    
+
     return accounts;
 });
 
