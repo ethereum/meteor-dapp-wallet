1f0677023580b6276a1e35961e37cc8b4b4c9fad
Ramesh Nair <ram@hiddentao.com>
2016-08-22 13:56:03 +0800

    fix collections persistence data

--- app/client/lib/collections.js
+++ app/client/lib/collections.js
@@ -3,38 +3,31 @@
 // we use {connection: null} to prevent them from syncing with our not existing Meteor server
 
 
-Wallets = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('wallets', {connection: null})
-);
-new PersistentMinimongo2(Wallets, 'ethereum_wallet');
 
-CustomContracts = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('custom-contracts', {connection: null})
-);
+Wallets = new Mongo.Collection('wallets', {connection: null});
+new PersistentMinimongo2(Wallets, 'ethereum_wallet');
+Wallets = new NetworkInfo.ProxyCollection(Wallets);
+
+CustomContracts = new Mongo.Collection('custom-contracts', {connection: null});
 new PersistentMinimongo2(CustomContracts, 'ethereum_wallet');
+CustomContracts = new NetworkInfo.ProxyCollection(CustomContracts);
 
 // Contains the transactions
-Transactions = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('transactions', {connection: null})
-);
+Transactions = new Mongo.Collection('transactions', {connection: null});
 new PersistentMinimongo2(Transactions, 'ethereum_wallet');
+Transactions = new NetworkInfo.ProxyCollection(Transactions);
 
 // Contains the pending confirmations
-PendingConfirmations = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('pending-confirmations', {connection: null})
-);
+PendingConfirmations = new Mongo.Collection('pending-confirmations', {connection: null});
 new PersistentMinimongo2(PendingConfirmations, 'ethereum_wallet');
+PendingConfirmations = new NetworkInfo.ProxyCollection(PendingConfirmations);
 
 // Contains the custom contract events
-Events = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('events', {connection: null})
-);
+Events = new Mongo.Collection('events', {connection: null});
 new PersistentMinimongo2(Events, 'ethereum_wallet');
+Events = new NetworkInfo.ProxyCollection(Events);
 
 // Contains Coin Information
-Tokens = new NetworkInfo.ProxyCollection(
-  new Mongo.Collection('tokens', {connection: null})
-);
+Tokens = new Mongo.Collection('tokens', {connection: null});
 new PersistentMinimongo2(Tokens, 'ethereum_wallet');
-
-
+Tokens = new NetworkInfo.ProxyCollection(Tokens);
