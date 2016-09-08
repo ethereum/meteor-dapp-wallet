40b11729f8b33d6c1647f801b356a5a94b94d841
Ramesh Nair <ram@hiddentao.com>
2016-09-08 17:09:22 +0800

    upgrade networkinfo pkg, auto-port old data

--- app/client/lib/collections.js
+++ app/client/lib/collections.js
@@ -6,28 +6,40 @@
 
 Wallets = new Mongo.Collection('wallets', {connection: null});
 new PersistentMinimongo2(Wallets, 'ethereum_wallet');
-Wallets = new NetworkInfo.ProxyCollection(Wallets);
+Wallets = new NetworkInfo.ProxyCollection(Wallets, {
+    portOldData: true
+});
 
 CustomContracts = new Mongo.Collection('custom-contracts', {connection: null});
 new PersistentMinimongo2(CustomContracts, 'ethereum_wallet');
-CustomContracts = new NetworkInfo.ProxyCollection(CustomContracts);
+CustomContracts = new NetworkInfo.ProxyCollection(CustomContracts, {
+    portOldData: true
+});
 
 // Contains the transactions
 Transactions = new Mongo.Collection('transactions', {connection: null});
 new PersistentMinimongo2(Transactions, 'ethereum_wallet');
-Transactions = new NetworkInfo.ProxyCollection(Transactions);
+Transactions = new NetworkInfo.ProxyCollection(Transactions, {
+    portOldData: true
+});
 
 // Contains the pending confirmations
 PendingConfirmations = new Mongo.Collection('pending-confirmations', {connection: null});
 new PersistentMinimongo2(PendingConfirmations, 'ethereum_wallet');
-PendingConfirmations = new NetworkInfo.ProxyCollection(PendingConfirmations);
+PendingConfirmations = new NetworkInfo.ProxyCollection(PendingConfirmations, {
+    portOldData: true
+});
 
 // Contains the custom contract events
 Events = new Mongo.Collection('events', {connection: null});
 new PersistentMinimongo2(Events, 'ethereum_wallet');
-Events = new NetworkInfo.ProxyCollection(Events);
+Events = new NetworkInfo.ProxyCollection(Events, {
+    portOldData: true
+});
 
 // Contains Coin Information
 Tokens = new Mongo.Collection('tokens', {connection: null});
 new PersistentMinimongo2(Tokens, 'ethereum_wallet');
-Tokens = new NetworkInfo.ProxyCollection(Tokens);
+Tokens = new NetworkInfo.ProxyCollection(Tokens, {
+    portOldData: true
+});
