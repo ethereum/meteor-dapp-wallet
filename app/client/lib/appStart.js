de638cfaa616bc0fe531706a3d746ba22a53f72b
Fabian Vogelsteller <fabian@frozeman.de>
2016-12-14 17:42:35 +0100

    fixed example contract

--- app/client/lib/appStart.js
+++ app/client/lib/appStart.js
@@ -3,6 +3,12 @@ if(location.hostname !== 'localhost' && location.hostname !== '127.0.0.1')
     Meteor.disconnect();
 
 
+// Make sure the example contract code is up to date
+var shortExample = Helpers.defaultContractExample.substr(Helpers.defaultContractExample.indexOf("\n\n")+2);
+if (localStorage.getItem('contractSource').indexOf(shortExample) !== -1) {
+    localStorage.setItem('contractSource', Helpers.defaultContractExample);
+}
+ 
 
 Meteor.Spinner.options = {
     lines: 17, // The number of lines to draw
