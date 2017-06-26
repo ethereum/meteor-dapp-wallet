b23f3cc50830c9d75426813fc2da924ea682f2e6
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2017-06-26 11:17:41 -0300

    build

--- build/bundle/programs/server/npm/node_modules/meteor/kadira_flow-router/node_modules/page/examples/partials/app.js
+++
@@ -1,9 +0,0 @@
-
-page('*', init.ctx);
-page('/partials/home', route.home);
-page('/partials/portfolio', route.portfolio);
-page('/partials/about', route.about);
-page('/partials/test', route.test);
-page('/partials/examples', route.examples);
-page('*', render.content);
-page();
