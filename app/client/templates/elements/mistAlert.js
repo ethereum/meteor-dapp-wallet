6474c4e487541fbd37fc3dffda6ef744f8bf2f31
Everton Fraga <ev@ethereum.org>
2017-12-04 15:08:38 -0500

    Removing console

--- app/client/templates/elements/mistAlert.js
+++ app/client/templates/elements/mistAlert.js
@@ -1,11 +1,7 @@
 
 var alertKey = 'alert_20171104-hidden';
-console.log('mistAlert started');
 Template['mist_alert'].onRendered(function(){
-    console.log(window.navigator, window.navigator.userAgent);
-
     TemplateVar.set('hidden', localStorage.getItem(alertKey));
-    console.log(TemplateVar.get('hidden'));
 });
 
 Template['mist_alert'].helpers({
