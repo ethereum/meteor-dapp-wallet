1472bde98015bec3d90907476a5d044795af780f
astro9113 <astro913@163.com>
2017-12-10 17:19:55 +0800

    update net

--- app/client/lib/ethereum/walletInterface.js
+++ app/client/lib/ethereum/walletInterface.js
@@ -34,7 +34,7 @@ web3.eth.getBlock(0, function(e, res){
     if(!e){
         switch(res.hash) {
             //cranelv genesis block hash
-            case '0x8e8e1504b2044d5fd89be7c2046b4fd080a7b2b0f870cd1f45af91115c84a663':
+            case '0xf395db10563a688d5878848b58926f82a251291bc9dba558ab66ae84c84fdeb8':
                 Session.set('network', 'main');
                 break;
             case '0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177':
