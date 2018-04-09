e844c6d2e61c8d717ac183594816f729b03e24c9
Daniel A. Nagy <nagydani@epointsystem.org>
2018-04-09 17:36:19 +0200

    Make QR-code ERC-681 compliant (#472)
    I am not entirely sure that this is the right place to put it as per MVC-pattern, but it seems appropriate.

--- app/client/templates/views/modals/qrCode.js
+++ app/client/templates/views/modals/qrCode.js
@@ -8,7 +8,7 @@ The template to display qrCode.
 
 Template['views_modals_qrCode'].onRendered(function(){
     if(this.data && this.data.address) {
-        var qrcodesvg = new Qrcodesvg(this.data.address, 'qrcode', 150, {"ecclevel" : 1});
+        var qrcodesvg = new Qrcodesvg("ethereum:" + this.data.address, 'qrcode', 150, {"ecclevel" : 1});
         qrcodesvg.draw({"method": "classic", "fill-colors":["#555","#555","#666"]}, {"stroke-width":1});
     }
 });
