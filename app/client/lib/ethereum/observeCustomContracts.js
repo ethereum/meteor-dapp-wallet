a6fcffaf240df9c6698548ef473b551af3260729
Ryan Ghods <ryan@ryanio.com>
2018-04-17 12:27:50 -0700

    Move commented out code to bottom of file

--- app/client/lib/ethereum/observeCustomContracts.js
+++ app/client/lib/ethereum/observeCustomContracts.js
@@ -1,59 +1,3 @@
-// var addLogWatching = function(newDocument){
-
-//     var contractInstance = new web3.eth.Contract(newDocument.jsonInterface, newDocument.address);
-//     var blockToCheckBack = (newDocument.checkpointBlock || 0) - ethereumConfig.rollBackBy;
-
-//     if(blockToCheckBack < 0)
-//         blockToCheckBack = 0;
-
-//     console.log('EVENT LOG:  Checking Custom Contract Events for '+ newDocument.address +' (_id: '+ newDocument._id + ') from block # '+ blockToCheckBack);
-
-//     // delete the last logs until block -500
-//     _.each(Events.find({_id: {$in: newDocument.contractEvents || []}, blockNumber: {$exists: true, $gt: blockToCheckBack}}).fetch(), function(log){
-//         if(log)
-//             Events.remove({_id: log._id});
-//     });
-
-//     var filter = contractInstance.allEvents({fromBlock: blockToCheckBack, toBlock: 'latest'});
-
-//     // get past logs, to set the new blockNumber
-//     var currentBlock = EthBlocks.latest.number;
-//     filter.get(function(error, logs) {
-//         if(!error) {
-//             // update last checkpoint block
-//             CustomContracts.update({_id: newDocument._id}, {$set: {
-//                 checkpointBlock: (currentBlock || EthBlocks.latest.number) - ethereumConfig.rollBackBy
-//             }});
-//         }
-//     });
-
-//     filter.watch(function(error, log){
-//         if(!error) {
-//             var id = Helpers.makeId('log', web3.sha3(log.logIndex + 'x' + log.transactionHash + 'x' + log.blockHash));
-
-//             if(log.removed) {
-//                 Events.remove(id);
-//             } else {
-//                 web3.eth.getBlock(log.blockHash, function(err, block){
-//                     if(!err) {
-
-//                         _.each(log.args, function(value, key){
-//                             // if bignumber
-//                             if((_.isObject(value) || value instanceof BigNumber) && value.toFormat) {
-//                                 value = value.toString(10);
-//                                 log.args[key] = value;
-//                             }
-//                         });
-
-//                         log.timestamp = block.timestamp;
-//                         Events.upsert(id, log);
-//                     }
-//                 });
-//             }
-//         }
-//     });
-// };
-
 /**
 Observe custom contacts
 
@@ -70,10 +14,10 @@ observeCustomContracts = function() {
     {}
   ).observe({
     /**
-        Will check if the contracts are on the current chain
+      Will check if the contracts are on the current chain
 
-        @method added
-        */
+      @method added
+    */
     added: function(newDocument) {
       // check if wallet has code
       web3.eth.getCode(newDocument.address, function(e, code) {
@@ -84,7 +28,7 @@ observeCustomContracts = function() {
             }
           });
 
-          // check for logs
+          // TODO: check for logs
           // addLogWatching(newDocument);
         } else if (!e) {
           // if there's no code, check the contract has a balance
@@ -96,7 +40,7 @@ observeCustomContracts = function() {
                 }
               });
 
-              // check for logs
+              // TODO: check for logs
               // addLogWatching(newDocument);
             } else if (!e) {
               CustomContracts.update(newDocument._id, {
@@ -111,3 +55,50 @@ observeCustomContracts = function() {
     }
   });
 };
+
+// TODO: convert from filters to subscriptions
+// var addLogWatching = function(newDocument){
+//     var contractInstance = new web3.eth.Contract(newDocument.jsonInterface, newDocument.address);
+//     var blockToCheckBack = (newDocument.checkpointBlock || 0) - ethereumConfig.rollBackBy;
+//     if(blockToCheckBack < 0)
+//         blockToCheckBack = 0;
+//     console.log('EVENT LOG:  Checking Custom Contract Events for '+ newDocument.address +' (_id: '+ newDocument._id + ') from block # '+ blockToCheckBack);
+//     // delete the last logs until block -500
+//     _.each(Events.find({_id: {$in: newDocument.contractEvents || []}, blockNumber: {$exists: true, $gt: blockToCheckBack}}).fetch(), function(log){
+//         if(log)
+//             Events.remove({_id: log._id});
+//     });
+//     var filter = contractInstance.allEvents({fromBlock: blockToCheckBack, toBlock: 'latest'});
+//     // get past logs, to set the new blockNumber
+//     var currentBlock = EthBlocks.latest.number;
+//     filter.get(function(error, logs) {
+//         if(!error) {
+//             // update last checkpoint block
+//             CustomContracts.update({_id: newDocument._id}, {$set: {
+//                 checkpointBlock: (currentBlock || EthBlocks.latest.number) - ethereumConfig.rollBackBy
+//             }});
+//         }
+//     });
+//     filter.watch(function(error, log){
+//         if(!error) {
+//             var id = Helpers.makeId('log', web3.sha3(log.logIndex + 'x' + log.transactionHash + 'x' + log.blockHash));
+//             if(log.removed) {
+//                 Events.remove(id);
+//             } else {
+//                 web3.eth.getBlock(log.blockHash, function(err, block){
+//                     if(!err) {
+//                         _.each(log.args, function(value, key){
+//                             // if bignumber
+//                             if((_.isObject(value) || value instanceof BigNumber) && value.toFormat) {
+//                                 value = value.toString(10);
+//                                 log.args[key] = value;
+//                             }
+//                         });
+//                         log.timestamp = block.timestamp;
+//                         Events.upsert(id, log);
+//                     }
+//                 });
+//             }
+//         }
+//     });
+// };
