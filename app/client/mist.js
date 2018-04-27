7cc204e6cde2ee4459932e5ad58c01f46f502ceb
hackyminer <hackyminer@gmail.com>
2018-04-27 21:37:46 +0900

    use EthTools's UNIT

--- app/client/mist.js
+++ app/client/mist.js
@@ -16,7 +16,7 @@ updateMistBadge = function() {
     );
 
     mist.menu.setBadge(
-      EthTools.formatBalance(balance, '0.0 a', 'ether') + ' ETH'
+      EthTools.formatBalance(balance, '0.0 UNIT', 'ether')
     );
   }
 };
@@ -68,7 +68,7 @@ updateMistMenu = function() {
           position: 3 + index,
           name: account.name,
           badge:
-            EthTools.formatBalance(account.balance, '0 a', 'ether') + ' ETH',
+            EthTools.formatBalance(account.balance, '0 UNIT', 'ether'),
           selected: location.pathname === '/account/' + account.address
         },
         function() {
