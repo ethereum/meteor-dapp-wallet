300b96d04781fc1d432d23f41cfbb129a1549eb6
Ryan Ghods <ryan@ryanio.com>
2018-07-18 15:26:45 -0700

    Updates

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -1,15 +1,33 @@
 if (window.ethereum) {
-  web3 = new Web3(window.ethereum);
+  start(window.ethereum);
 } else {
   // Set default provider
-  web3 = new Web3('ws://localhost:8546');
+  web3.setProvider('ws://localhost:8546');
 
   // Request Ethereum Provider (EIP 1102)
   window.addEventListener('message', event => {
     if (event.data && event.data.type === 'ETHEREUM_PROVIDER_SUCCESS') {
-      // Set provider
-      web3.setProvider(window.ethereum);
+      if (window.ethereum) {
+        start(window.ethereum);
+      }
     }
   });
   window.postMessage({ type: 'ETHEREUM_PROVIDER_REQUEST' }, this.origin);
 }
+
+const start = ethereum => {
+  // Set provider
+  web3.setProvider(ethereum);
+
+  // Restart app on certain events
+  ethereum.on('connect', startApp);
+  ethereum.on('networkChanged', startApp);
+  ethereum.on('accountsChanged', startApp);
+
+  // Go!
+  startApp();
+};
+
+const startApp = () => {
+  connectToNode();
+};
