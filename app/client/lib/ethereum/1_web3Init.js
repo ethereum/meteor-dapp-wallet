72059fd02ca05accd7aa9159328e2d52e2de5206
Ryan Ghods <ryan@ryanio.com>
2018-09-13 13:38:15 -0700

    Updates

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -1,23 +1,8 @@
 if (window.ethereum) {
   web3 = new Web3(window.ethereum);
+  // Enable full provider
+  window.ethereum.enable();
 } else {
-  // Use local websocket provider if available
-  try {
-    const provider = 'ws://localhost:8546';
-    const socket = new WebSocket(provider);
-    socket.addEventListener('open', event => {
-      web3 = new Web3(provider);
-    });
-  } catch (error) {
-    // Local provider is unavailable.
-    // Request Ethereum Provider (EIP 1102)
-    window.addEventListener('message', event => {
-      if (event.data && event.data.type === 'ETHEREUM_PROVIDER_SUCCESS') {
-        if (window.ethereum) {
-          web3 = new Web3(provider);
-        }
-      }
-    });
-    window.postMessage({ type: 'ETHEREUM_PROVIDER_REQUEST' }, this.origin);
-  }
+  // Use local websocket provider
+  web3 = new Web3('ws://localhost:8546');
 }
