7bd062f4240e176b0d389de4a8ab949a7820b705
Ryan Ghods <ryan@ryanio.com>
2018-10-27 18:10:06 +0200

    enable => requestAccounts

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -1,7 +1,7 @@
 if (window.ethereum) {
   web3 = new Web3(window.ethereum);
-  // Enable full provider
-  window.ethereum.enable();
+  // Request accounts
+  ethereum.send('eth_requestAccounts');
 } else {
   // Use local websocket provider
   web3 = new Web3('ws://localhost:8546');
